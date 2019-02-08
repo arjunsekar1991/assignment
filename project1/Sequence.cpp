7a2f4f80dd9f7276dbdd9354307d9b2189d0a853
Arjun <arjunsekar1991@gmail.com>
2019-02-07 20:57:31 -0500

    working version

---
+++ project1/Sequence.cpp
@@ -0,0 +1,73 @@
+//#include "Sequence.h"
+#include "pch.h"
+
+//using namespace std;
+Sequence::Sequence(size_type sz)
+{
+	cout << "constructor started";
+}
+
+/*Sequence::Sequence(const Sequence& s)
+{
+}
+
+Sequence::~Sequence()
+{
+}
+
+Sequence& Sequence::operator=(const Sequence& s)
+{
+}
+
+Sequence::value_type& Sequence::operator[](size_type position)
+{
+}
+
+Sequence::value_type& Sequence::at(size_type position)
+{
+}
+
+void Sequence::push_back(const value_type& value)
+{
+}
+
+void Sequence::pop_back()
+{
+}
+
+void Sequence::insert(size_type position, value_type value)
+{
+}
+
+const Sequence::value_type& Sequence::front() const
+{
+}
+
+const Sequence::value_type& Sequence::back() const
+{
+}
+
+bool Sequence::empty() const
+{
+}
+
+Sequence::size_type Sequence::size() const
+{
+}
+
+void Sequence::clear()
+{
+}
+
+void Sequence::erase(size_type position, size_type count)
+{
+}
+
+ostream& Sequence::print(ostream& os)
+{
+}
+
+
+ostream& operator<<(ostream& os, Sequence& s)
+{
+}*/
---
+++ project1/project1.cpp
@@ -0,0 +1,42 @@
+// project1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include "pch.h"
+#include <iostream>
+#include <iomanip>
+
+
+
+using namespace std;
+
+void testCopyConstructor(Sequence);
+
+int main()
+{
+	try
+	{
+		{
+			Sequence data(4);
+		/*	data[0] = 100;
+			data[1] = 200;
+			cout << "before calling testCopyConstructor: " << data << endl;
+			testCopyConstructor(data);
+			cout << "after calling testCopyConstructor: " << data << endl;*/
+		}
+
+		{
+		/*	Sequence data(4);
+			data[5] = 100;*/
+		}
+	}
+	catch (exception& e)
+	{
+		cout << "Exception: " << e.what() << endl;
+	}
+}
+
+void testCopyConstructor(Sequence s)
+{
+/*	cout << "inside testCopyConstructor: " << s << endl;
+	s[0] = -1;*/
+}
